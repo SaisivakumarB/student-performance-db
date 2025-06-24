6bfb2356ce3ed371713c0e42715090569d2a5911
B Sai Sivakumar <bssk408@gmail.com>
2025-06-23 19:38:24 -0500

    Add Data for student performance database

---
+++ data/sample_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO Students (name, email, dob, gender)
+VALUES 
+('Sai Sivakumar', 'ssk@github.com', '2001-03-15', 'Male'),
+('Sivakumar', 'sk@github.com', '2000-07-22', 'Male');
+
+INSERT INTO Courses (course_name, credits)
+VALUES
+('Mathematics', 3),
+('History', 2);
+
+INSERT INTO Enrollments (student_id, course_id, semester)
+VALUES
+(1, 1, 'Fall2024'),
+(2, 2, 'Fall2024');
+
+INSERT INTO Marks (enroll_id, midterm, final, total)
+VALUES
+(1, 40, 50, 90),
+(2, 35, 45, 80);
+
+INSERT INTO Attendance (enroll_id, total_classes, attended)
+VALUES
+(1, 40, 38),
+(2, 40, 36);
